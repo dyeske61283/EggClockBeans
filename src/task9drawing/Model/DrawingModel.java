ffe1ecf1945f9b973f22a8c3537e917a6966321d
kevin <kevin@kevin-LIFEBOOK-SH531>
2016-12-01 16:58:25 +0100

    First commit to Task9

---
+++ src/task9drawing/Model/DrawingModel.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package task9drawing.Model;
+
+/**
+ *
+ * @author kevin
+ */
+public class DrawingModel
+{
+  public DrawingModel()
+  {
+
+  }
+}
